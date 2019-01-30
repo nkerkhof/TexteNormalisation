d97a3287fc2543502e6a00a5b22f2f367b1ddb87
Unknown <simon.bonnaud@gmail.com>
2019-01-30 14:14:50 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/v1.py
+++ src/v1.py
@@ -107,16 +107,16 @@ def txt_to_train_data(txt):
 #entrée : path_origine / path_dest_txt_test_after / path_dest_txt_test_after_norm2
 
 #start = time.time()
-#delStartAndEnd("data/rawdata/gutenberg/8692-0.txt","data/test/8692_v1.txt")
-#txt = loadFiletxt("data/test/8692_v1.txt")
-#txt = sentencePerLine(txt)
-#txt = delEmptyLines(txt)
+delStartAndEnd("8693-0-tokenized.txt","8692_v1.txt")
+txt = loadFiletxt("8692_v1.txt")
+txt = sentencePerLine(txt)
+txt = delEmptyLines(txt)
 
-#f=open('data/test/test_after.txt','w+')
-#f.write(txt)
-#f.close()
+f=open('8692_v1.txt','w')
+
+f.write(txt)
+f.close()
 #print(time.time() - start)
-
 
 
 
@@ -147,6 +147,79 @@ f.close()
 
 
 
+
+
+
+
+# Define sampling models
+encoder_model = Model(encoder_inputs, encoder_states)
+
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs] + decoder_states)
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_char_index = dict(
+    (i, char) for char, i in input_token_index.items())
+reverse_target_char_index = dict(
+    (i, char) for char, i in target_token_index.items())
+
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index['\t']] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == '\n' or
+           len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+print('Decoded sentence:', decoded_sentence)
+'''
 
 
 
